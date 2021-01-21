580b0393efcdc31928c57ed6bdf733444027d1d6
ulugbekabirov <ulugbekabirov@gmail.com>
2021-01-21 20:21:46 +0500

    Added classes for auth

---
+++ InternshipBe/IdentityServer/Authentication/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Authentication
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+    }
+}
