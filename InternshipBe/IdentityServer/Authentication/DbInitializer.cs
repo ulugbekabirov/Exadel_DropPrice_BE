25651f48a6320923df007959709ca61a220fee02
ulugbekabirov <ulugbekabirov@gmail.com>
2021-01-22 20:32:30 +0500

    Seeding database with users

---
+++ InternshipBe/IdentityServer/Authentication/DbInitializer.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer.Authentication
+{
+    public class DbInitializer
+    {
+        private static readonly string password = "Qwerty123!";
+
+        public static async Task InitializeAsync(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+
+            if (userManager.Users.Any())
+            {
+                return;
+            }
+            if (!roleManager.Roles.Any())
+            {
+                await roleManager.CreateAsync(new IdentityRole("Admin"));
+                await roleManager.CreateAsync(new IdentityRole("Moderator"));
+                await roleManager.CreateAsync(new IdentityRole("User"));
+            }
+
+            ApplicationUser admin = new ApplicationUser()
+            {
+                UserName = "admin@exadel.com",
+                Email = "admin@exadel.com",
+            };
+
+            ApplicationUser moderator = new ApplicationUser()
+            {
+                UserName = $"moderator@exadel.com",
+                Email = $"moderator@exadel.com",
+            };
+
+            await userManager.CreateAsync(moderator, password);
+            await userManager.CreateAsync(admin, password);
+
+            for (int i = 0; i < 1200; i++)
+            {
+                ApplicationUser user = new ApplicationUser()
+                {
+                    UserName = $"user{i}@exadel.com",
+                    Email = $"user{i}@exadel.com",
+                };
+                await userManager.CreateAsync(user, password);
+                await userManager.AddToRoleAsync(user, "User");
+            }
+        }
+    }
+}
--- InternshipBe/IdentityServer/Program.cs
+++ InternshipBe/IdentityServer/Program.cs
@@ -1,5 +1,8 @@
+using IdentityServer.Authentication;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -11,9 +14,27 @@ namespace IdentityServer
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    await DbInitializer.InitializeAsync(userManager, rolesManager);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while seeding the database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
