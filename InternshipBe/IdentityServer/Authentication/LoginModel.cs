da2131a9aea23eefc373d0c2027474f7ea444f62
ulugbekabirov <ulugbekabirov@gmail.com>
2021-01-22 16:37:46 +0500

    data annotation for email added

--- InternshipBe/IdentityServer/Authentication/LoginModel.cs
+++ InternshipBe/IdentityServer/Authentication/LoginModel.cs
@@ -9,6 +9,7 @@ namespace IdentityServer.Authentication
     public class LoginModel
     {
         [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email adress")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Password is required")]
