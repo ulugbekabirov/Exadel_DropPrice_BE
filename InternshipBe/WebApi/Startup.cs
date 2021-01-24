3d3f8ff1b3bd3b6802ed7411d9df708ae0842638
= <romanstat@mail.ru>
2021-01-24 10:47:13 +0300

    Added cors to WebApi Startup

--- InternshipBe/WebApi/Startup.cs
+++ InternshipBe/WebApi/Startup.cs
@@ -12,13 +12,11 @@ namespace WebApi
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -27,6 +25,8 @@ namespace WebApi
             }
 
             app.UseRouting();
+
+            app.UseCors(builder => builder.AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
