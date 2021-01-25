ca890d1833378a7e3aa5db88d8259226ab68257a
ulugbekabirov <ulugbekabirov@gmail.com>
2021-01-25 23:21:59 +0500

    added ticket entity

---
+++ InternshipBe/DAL/Entities/Ticket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    class Ticket
+    {
+        public int Id { get; set; }
+        public Discount Discount { get; set; }
+        public User User { get; set; }
+        public string Description { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
