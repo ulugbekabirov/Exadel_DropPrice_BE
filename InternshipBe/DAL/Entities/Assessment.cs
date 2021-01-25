b4bf5e2ff27a49180cff738228a63574f1b5017a
= <romanstat@mail.ru>
2021-01-25 21:18:22 +0300

    Added Assesment Entity

---
+++ InternshipBe/DAL/Entities/Assessment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    class Assessment
+    {
+        public int Id { get; set; }
+        public User User { get; set; }
+        public Discount Discount { get; set; }
+    }
+}
