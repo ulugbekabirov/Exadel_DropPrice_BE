417326882fdb0d6ca69f6802fa162a4872096065
= <romanstat@mail.ru>
2021-01-25 20:04:27 +0300

    Added Discount entity

---
+++ InternshipBe/DAL/Entities/Discount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    class Discount
+    {
+        public int Id { get; set; }
+        public int IdVendor { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DiscountAmount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool ActivityStatus { get; set; }
+        public string Promocode { get; set; }
+        public string ImagePath { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+
+        public Discount()
+        {
+            Tags = new List<Tag>();
+        }
+    }
+}
