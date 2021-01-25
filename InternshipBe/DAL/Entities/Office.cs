ead9a46f8c7b6877c7429beec2d8a48b70b24005
ulugbekabirov <ulugbekabirov@gmail.com>
2021-01-25 22:05:58 +0500

    added office entity

--- InternshipBe/DAL/Entities/Office.cs
+++ InternshipBe/DAL/Entities/Office.cs
@@ -6,9 +6,12 @@ using System.Threading.Tasks;
 
 namespace DAL.Entities
 {
-    class Tag  
+    class Office
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
