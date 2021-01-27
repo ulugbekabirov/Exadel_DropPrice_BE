d53c2dd4e93f9b9725475ded671cf9a864268ad6
= <romanstat@mail.ru>
2021-01-27 17:49:36 +0300

    Added UserDTO

---
+++ InternshipBe/BL/DTO/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.DTO
+{
+    class UserDTO
+    {
+        public int Id { get; set; }
+
+        public string[] Roles { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
