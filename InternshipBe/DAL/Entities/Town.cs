34cc8063af810c000fac455abc5bbc26f0a57cad
= <romanstat@mail.ru>
2021-01-27 14:40:47 +0300

    Added Town Entity

--- InternshipBe/DAL/Entities/Town.cs
+++ InternshipBe/DAL/Entities/Town.cs
@@ -7,27 +7,17 @@ using System.Threading.Tasks;
 
 namespace DAL.Entities
 {
-    public class PointOfSale
+    class Town
     {
-        public PointOfSale()
-        {
-            Discounts = new List<Discount>();
-        }
-
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; }
-
-        [Required]
-        public string Address { get; set; }
+        public int Name { get; set; }
 
         [Required]
         public double Latitude { get; set; }
 
         [Required]
         public double Longitude { get; set; }
-
-        public ICollection<Discount> Discounts { get; set; }
     }
 }
