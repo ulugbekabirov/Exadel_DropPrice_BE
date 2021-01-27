296e07727308af1f646037f3c25337123dcbc792
= <romanstat@mail.ru>
2021-01-27 20:30:46 +0300

    Added IDiscountRepository and DiscountRepository

---
+++ InternshipBe/DAL/Interfaces/IDiscountRepository.cs
@@ -0,0 +1,18 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    interface IDiscountRepository : IRepository<Discount>
+    {
+        IEnumerable<Discount> GetDiscount(int skip, int take, double latitude, double longitude);
+
+        IEnumerable<Discount> SearchDiscountsByDiscountName(int skip, int take, double latitude, double longitude, string DiscountName);
+
+        IEnumerable<Discount> SearchDiscountsByTagName(int skip, int take, double latitude, double longitude, string TagName);
+    }
+}
