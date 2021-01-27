7b58c542fdc9c4b874efcd94e4fda00540da73ed
ulugbekabirov <ulugbekabirov@gmail.com>
2021-01-27 19:13:53 +0500

    changed entities signature to protected

--- InternshipBe/DAL/Repositories/Repository.cs
+++ InternshipBe/DAL/Repositories/Repository.cs
@@ -12,7 +12,7 @@ namespace DAL.Repositories
     public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
     {
         protected readonly ApplicationDbContext _context;
-        private readonly DbSet<TEntity> _entities;
+        protected readonly DbSet<TEntity> _entities;
 
         public Repository(ApplicationDbContext context)
         {
