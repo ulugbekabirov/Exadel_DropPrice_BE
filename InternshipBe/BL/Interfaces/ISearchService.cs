62129164b7a126078f2b575d5f07c24df17e1b9a
ulugbekabirov <ulugbekabirov@gmail.com>
2021-01-28 23:59:21 +0500

    removed unnessary imports from BL

--- InternshipBe/BL/Interfaces/ISearchService.cs
+++ InternshipBe/BL/Interfaces/ISearchService.cs
@@ -1,9 +1,5 @@
 ﻿using BL.DTO;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BL.Interfaces
 {
--- InternshipBe/BL/Services/SearchService.cs
+++ InternshipBe/BL/Services/SearchService.cs
@@ -1,11 +1,7 @@
 ﻿using BL.DTO;
 using BL.Interfaces;
-using DAL.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BL.Services
 {
