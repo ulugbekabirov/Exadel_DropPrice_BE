feb81a6077f03972616080fb9e1fcc507622810a
= <romanstat@mail.ru>
2021-01-29 10:34:32 +0300

    Added ITownService

---
+++ InternshipBe/BL/Interfaces/ITownService.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Interfaces
+{
+    public interface ITownService
+    {
+        IEnumerable<Town> GetTown();
+    }
+}
