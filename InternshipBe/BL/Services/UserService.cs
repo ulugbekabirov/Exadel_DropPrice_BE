d0c578e84ba164f526b984eca21876c01e1c38c5
= <romanstat@mail.ru>
2021-02-01 13:38:57 +0300

    User service and repository reimpelemted

--- InternshipBe/BL/Services/UserService.cs
+++ InternshipBe/BL/Services/UserService.cs
@@ -21,13 +21,9 @@ namespace BL.Services
 
         public async Task<UserDTO> GetUserInfoAsync(User user)
         {
-            var userRoles = await _repository.GetUserRolesAsync(user.Id);
+            var userDTO = _mapper.Map<UserDTO>(user);
 
-            var userDTO = _mapper.Map<User, UserDTO>(user);
-
-            userDTO.Roles = userRoles.ToArray();
-
-            return userDTO;
+            return _mapper.Map(await _repository.GetUserRolesAsync(user.Id), userDTO);
         }
     }
 }
--- InternshipBe/DAL/Interfaces/IUserRepository.cs
+++ InternshipBe/DAL/Interfaces/IUserRepository.cs
@@ -1,4 +1,5 @@
 ﻿using DAL.Entities;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -6,6 +7,6 @@ namespace DAL.Interfaces
 {
     public interface IUserRepository : IRepository<User>
     {
-        Task<IQueryable<string>> GetUserRolesAsync(int userId);
+        Task<IEnumerable<string>> GetUserRolesAsync(int userId);
     }
 }
