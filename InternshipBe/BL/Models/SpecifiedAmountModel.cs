81fa0790d63859b3129dbba21f9bdde335dbbcd1
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-02 17:22:30 +0500

    removed unnesseary imports

--- InternshipBe/BL/Models/SpecifiedAmountModel.cs
+++ InternshipBe/BL/Models/SpecifiedAmountModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BL.Models
+﻿namespace BL.Models
 {
     public class SpecifiedAmountModel
     {
