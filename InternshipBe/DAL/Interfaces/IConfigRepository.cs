5df5c3b43b9ffcd64d01f3d53fd0ce16ba4119b6
Андрей Драгун <apocaliptik450@gmail.com>
2021-02-03 14:33:51 +0300

    add ConfigVarieblse

---
+++ InternshipBe/DAL/Interfaces/IConfigRepository.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    interface IConfigRepository : IRepository<ConfigVariebles>
+    {
+        Task<int> GetRadius();
+    }
+}
---
+++ InternshipBe/DAL/Repositories/ConfigRepository.cs
@@ -0,0 +1,27 @@
+﻿using DAL.DataContext;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class ConfigRepository : Repository<ConfigVariebles>, IConfigRepository
+    {
+        public ConfigRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public Task<Ticket> GetRadius()
+        {
+            var radius = _context.
+
+            return radius;
+        }
+    }
+}
