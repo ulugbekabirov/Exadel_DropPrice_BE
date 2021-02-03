23659c5a1881ea337bbbbc6fd87c4d22d70263a0
= <romanstat@mail.ru>
2021-02-03 12:10:17 +0300

    Added Ticket repository and service

---
+++ InternshipBe/BL/Services/TicketService.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using BL.DTO;
+using BL.Interfaces;
+using DAL.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Services
+{
+    public class TicketService : ITicketService
+    {
+        private readonly ITicketRepository _ticketRepository;
+        private readonly IMapper _mapper;
+
+        public TicketService(ITicketRepository repository, IMapper mapper)
+        {
+            _ticketRepository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<TicketDTO> GetTicket(int discountId, User user)
+        {
+            var userTicket = await _ticketRepository.GetTicketForUser(discountId, user);
+
+            return _mapper.Map<TicketDTO>(userTicket);
+        }
+    }
+}
---
+++ InternshipBe/DAL/Repositories/TicketRepository.cs
@@ -0,0 +1,42 @@
+﻿using DAL.DataContext;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class TicketRepository : Repository<Ticket>, ITicketRepository
+    {
+        public TicketRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public async Task<Ticket> GetTicketForUser(int discountId, User user)
+        {
+            var ticket = await _context.Tickets.SingleOrDefaultAsync(d => d.DiscountId == discountId && d.UserId == user.Id && d.OrderDate == DateTime.Today);
+
+            if (ticket is null)
+            {
+                var newTicket = new Ticket()
+                {
+                    DiscountId = discountId,
+                    UserId = user.Id,
+                    OrderDate = DateTime.Now,
+                    User = user,
+                    Discount = await _context.Discounts.SingleAsync(d => d.Id == discountId),
+                };
+
+                await CreateAsync(newTicket);
+                return newTicket;
+            }
+
+            return ticket;
+        }
+    }
+}
