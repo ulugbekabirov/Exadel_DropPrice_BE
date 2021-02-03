6ee13791cb3e87b97c50ebd2af1e25d9808190fa
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-03 20:34:32 +0500

    added vendor by id method

--- InternshipBe/BL/Services/VendorService.cs
+++ InternshipBe/BL/Services/VendorService.cs
@@ -20,6 +20,15 @@ namespace BL.Services
             _vendorRepository = vendorRepository;
             _mapper = mapper;
         }
+
+        public async Task<VendorDTO> GetVendorByIdAsync(int id)
+        {
+            var vendor = await _vendorRepository.GetByIdAsync(id);
+
+            return _mapper.Map<VendorDTO>(vendor);
+
+        }
+
         public async Task<IEnumerable<VendorDTO>> GetVendorsAsync()
         {
             var vendors = await _vendorRepository.GetAllAsync();
