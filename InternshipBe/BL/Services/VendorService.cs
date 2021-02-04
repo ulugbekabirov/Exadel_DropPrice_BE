7a0560b018d320833f17f52e9e7cc7b279ff0d7a
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-04 23:45:39 +0500

    removed unessary usings

--- InternshipBe/BL/Services/VendorService.cs
+++ InternshipBe/BL/Services/VendorService.cs
@@ -3,10 +3,8 @@ using BL.DTO;
 using BL.Interfaces;
 using DAL.Entities;
 using DAL.Interfaces;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BL.Services
