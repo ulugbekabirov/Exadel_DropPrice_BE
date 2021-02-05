44b874d94476e70df0322a7af86ce93e758d5dc2
= <romanstat@mail.ru>
2021-02-05 13:51:21 +0300

    Add DiscountExtension

---
+++ InternshipBe/BL/Extensions/DiscountExtension.cs
@@ -0,0 +1,45 @@
+﻿using BL.DTO;
+using BL.Models;
+using DAL.Entities;
+using GeoCoordinatePortable;
+using Shared.Extensions;
+using System.Linq;
+
+namespace BL.Extensions
+{
+    public static class DiscountExtension
+    {
+        public static DiscountModel CreateDiscountModel(this Discount discount, GeoCoordinate location, int userId)
+        {
+            var discountModel = new DiscountModel()
+            {
+                Discount = discount,
+                DiscountRating = discount.DiscountRating(),
+                PointOfSaleDTO = discount.GetPointOfSaleDTO(location),
+                IsSaved = discount.IsSavedDiscount(userId),
+                Tags = discount.GetTags(),
+            };
+
+            return discountModel;
+        }
+
+        public static PointOfSaleDTO GetPointOfSaleDTO(this Discount discount, GeoCoordinate location)
+        {
+            if (discount.PointOfSales.Count == 0)
+            {
+                return null;
+            }
+
+            var pointOfSaleDTO = discount.PointOfSales
+                .Select(p => new PointOfSaleDTO()
+                {
+                    Address = p.Address,
+                    DistanceInMeters = (int)location.GetDistanceTo(new GeoCoordinate(p.Latitude, p.Longitude))
+                })
+                .OrderBy(p => p.DistanceInMeters)
+                .FirstOrDefault();
+
+            return pointOfSaleDTO;
+        }
+    }
+}
