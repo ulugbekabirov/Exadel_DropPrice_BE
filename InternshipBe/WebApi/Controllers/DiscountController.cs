2f300f530bb11522d15e4373c23898e6494adf96
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-05 16:44:51 +0500

    changed createTicket endpoint

--- InternshipBe/WebApi/Controllers/DiscountController.cs
+++ InternshipBe/WebApi/Controllers/DiscountController.cs
@@ -13,11 +13,13 @@ namespace WebApi.Controllers
     public class DiscountController : ControllerBase
     {
         private readonly IDiscountService _discountService;
+        private readonly ITicketService _ticketService;
         private readonly UserManager<User> _userManager;
 
-        public DiscountController(IDiscountService service, UserManager<User> userManager)
+        public DiscountController(IDiscountService discountService, ITicketService ticketService, UserManager<User> userManager)
         {
-            _discountService = service;
+            _discountService = discountService;
+            _ticketService = ticketService;
             _userManager = userManager;
         }
 
@@ -38,7 +40,13 @@ namespace WebApi.Controllers
         {
             return Ok(await _discountService.SearchAsync(searchQuery));
         }
-        
+
+        [HttpGet("{id}/createTicket")]
+        public async Task<IActionResult> CreateTicket(int id)
+        {
+            return Ok(await _ticketService.GetOrCreateTicketAsync(id, await _userManager.FindByNameAsync(User.Identity.Name)));
+        }
+
         [HttpPut("{id}/save")]
         public async Task<IActionResult> SaveDiscount([FromBody]int discountId)
         {
