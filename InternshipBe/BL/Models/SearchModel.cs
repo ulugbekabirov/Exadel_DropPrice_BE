960fd646f8cd7b9d999b774511d2e4176786e82d
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-05 22:34:13 +0500

    added search model

---
+++ InternshipBe/BL/Models/SearchModel.cs
@@ -0,0 +1,16 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Models
+{
+    public class SearchModel : SortModel
+    {
+        public string SearchQuery { get; set; }
+
+        public string[] Tags { get; set; } = Array.Empty<string>();
+    }
+}
