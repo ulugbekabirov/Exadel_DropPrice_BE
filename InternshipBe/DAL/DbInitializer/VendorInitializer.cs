a61b49a4778c53df454bf66286900bcdb5a4c6fd
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-06 22:46:54 +0500

    added evos

--- InternshipBe/DAL/DbInitializer/VendorInitializer.cs
+++ InternshipBe/DAL/DbInitializer/VendorInitializer.cs
@@ -14,7 +14,8 @@ namespace DAL.DbInitializer
 
         public void InitializeVendors()
         {
-            AddVendor("Coffee", "The best coffee", "vendorexadel@gmail.com", "ул. Академика Купревича 3, Минск 220141", "+375447777777", "www.instagram.com/vendor");
+            AddVendor("Evos Lavash Center", @"Если Вам нужны кафе Evos в Ташкенте, здесь есть то, что Вы ищете! Справочный каталог с информацией о фастфудах Evos в Ташкенте: телефоны, адреса, местонахождения на карте Ташкента с режимами работы, ссылками на их соц. сети и кратким описанием – всё это Вы можете найти на страницах нашего сайта. Сеть Evos ждет Вас, чтобы предложить свои блюда. Полный список, удобный поиск, актуальная информация о сети кафе Evos в Ташкенте в Узбекистане",
+                "vendorexadel@gmail.com", @"Мирзо-Улугбекский ул.МУХАММАДА ЮСУФА, 1А", "+998 71 2031212", "https://www.instagram.com/evosuzbekistan/");
             AddVendor("Food", "The best food", "vendorexadel@gmail.com", "ул. Максима Горького 1/1, Гродно 230023", "+375447777777", "www.instagram.com/vendor");
             AddVendor("Reebok", "The best snickers", "vendorexadel@gmail.com","Inkubator Technologiczny, Żurawia 71, 15-540 Białystok, Польша", "+375447777777", "www.instagram.com/vendor");
         }
