217f2f270867d4371b36a2e9ab4499d9c5388e4e
= <romanstat@mail.ru>
2021-02-06 22:26:10 +0300

    Added KFC discounts

--- InternshipBe/DAL/DbInitializer/DiscountInitializer.cs
+++ InternshipBe/DAL/DbInitializer/DiscountInitializer.cs
@@ -35,6 +35,19 @@ namespace DAL.DbInitializer
                 _context.Vendors.Find(2),
                 _context.PointOfSales.Where(p => p.Name == "Пицца темпо Громова" || p.Name == "Пицца темпо Бобруйская" || p.Name == "Пицца темпо Мстиславца").ToList(),
                 _context.Tags.Where(t => t.Name == "Доставка" || t.Name == "Пицца" || t.Name == "Выгодно" || t.Name == "Уютное место").ToList());
+
+            //KFC
+            AddDiscount(3, $"Баскет дуэт со скидкой 50% по купону 5050", "С 6 ферваля компанейский Баскет Дуэт – минус 50 %! В одном баскете собрали для вас: сочные кусочки курицы, острые крылышки, нежные стрипсы и картофель фри.Есть смысл прийти с другом.",
+                50, DateTime.Now, DateTime.Now.Add(new TimeSpan(100, 0, 0, 0)), true, "5050",
+                _context.Vendors.Find(3),
+                _context.PointOfSales.Where(p => p.Name == "KFC Минск" || p.Name == "KFC Гомель" || p.Name == "KFC Ташкент" || p.Name == "KFC Warszawa" || p.Name == "KFC Walnut Creek").ToList(),
+                _context.Tags.Where(t => t.Name == "Доставка" || t.Name == "Напитки" || t.Name == "Выгодно" || t.Name == "Курица" || t.Name == "Фастфуд" || t.Name == "KFC"  || t.Name == "Быстро").ToList());
+
+            AddDiscount(3, $"Сандерс баскет", "Даже в плохую и холодную погоду KFC знает, как поднять тебе настроение! Целую неделю. Баскет всего за 7.70 руб.по купону 7070. Акция действует во всех ресторанах KFC Беларуси. Оформить заказ можно навынос, через окно Драйва или экспресс - окно. Ждем вас!",
+                50, DateTime.Now, DateTime.Now.Add(new TimeSpan(100, 0, 0, 0)), true, "7070",
+                _context.Vendors.Find(3),
+                _context.PointOfSales.Where(p => p.Name == "KFC Минск" || p.Name == "KFC Гомель" || p.Name == "KFC Ташкент" || p.Name == "KFC Warszawa" || p.Name == "KFC Walnut Creek").ToList(),
+                _context.Tags.Where(t => t.Name == "Доставка" || t.Name == "Напитки" || t.Name == "Выгодно" || t.Name == "Курица" || t.Name == "Фастфуд" || t.Name == "KFC" || t.Name == "Быстро").ToList());
         }
 
         public void AddDiscount(int vendorId, string name, string description, int discountAmount, DateTime startDate, DateTime endDate, bool activityStatus,
