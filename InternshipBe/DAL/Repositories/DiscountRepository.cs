3344d0e1c750a5b020741351c8e9403dde1f26f9
= <romanstat@mail.ru>
2021-02-06 18:24:51 +0300

    renamed field DiscountRepository

--- InternshipBe/DAL/Repositories/DiscountRepository.cs
+++ InternshipBe/DAL/Repositories/DiscountRepository.cs
@@ -19,8 +19,9 @@ namespace DAL.Repositories
 
         public async Task<IEnumerable<Discount>> GetClosestActiveDiscountsAsync(GeoCoordinate location)
         {
-            var x = await _entities.Where(d => d.ActivityStatus == true).ToListAsync();
-                return x.Where(d => d.PointOfSales.Min(p => location.GetDistanceTo(new GeoCoordinate(p.Latitude, p.Longitude))) < 50000);
+            var activeDiscounts = await _entities.Where(d => d.ActivityStatus == true).ToListAsync();
+
+            return activeDiscounts.Where(d => d.PointOfSales.Min(p => location.GetDistanceTo(new GeoCoordinate(p.Latitude, p.Longitude))) < 50000);
         }
 
         public async Task<SavedDiscount> GetSavedDiscountAsync(int discountId, int userId)
@@ -49,7 +50,7 @@ namespace DAL.Repositories
 
         public async Task<IEnumerable<Discount>> SearchDiscounts(string searchQuery, string[] tags)
         {
-            var searchResults = _context.Discounts.AsQueryable().Where(d=>d.ActivityStatus==true);
+            var searchResults = _context.Discounts.AsQueryable().Where(d => d.ActivityStatus == true);
 
             if (!string.IsNullOrEmpty(searchQuery))
             {
