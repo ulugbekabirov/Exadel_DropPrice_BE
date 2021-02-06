48a2999f28cc8e305a3ff2be22ee87539503dc3d
= <romanstat@mail.ru>
2021-02-06 18:17:49 +0300

    Removed enum and sort from SortModel

--- InternshipBe/BL/Models/SortModel.cs
+++ InternshipBe/BL/Models/SortModel.cs
@@ -1,33 +1,7 @@
-﻿using BL.DTO;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace BL.Models
+﻿namespace BL.Models
 {
-    public enum Sorts
-    {
-        DiscountRatingAsc,
-        DiscountRatingDesc,
-        DistanceAsc,
-        DistanceDesc,
-        AlphabetAsc,
-        AlphabetDesc,
-    }
-
     public class SortModel : LocationModel
     {
         public string SortBy { get; set; } = "DistanceAsc";
-
-        public static IEnumerable<DiscountDTO> SortDiscountsBy(DiscountDTO[] discounts, Sorts sortBy)
-            => sortBy switch
-            {
-                Sorts.AlphabetAsc => discounts.OrderBy(d => d.DiscountName).ThenBy(d => d.DistanceInMeters),
-                Sorts.AlphabetDesc => discounts.OrderByDescending(d => d.DiscountName).ThenBy(d => d.DistanceInMeters),
-                Sorts.DiscountRatingAsc => discounts.OrderBy(d => d.DiscountRating).ThenBy(d => d.DistanceInMeters),
-                Sorts.DiscountRatingDesc => discounts.OrderByDescending(d => d.DiscountRating).ThenBy(d => d.DistanceInMeters),
-                Sorts.DistanceAsc => discounts.OrderBy(d => d.DistanceInMeters),
-                Sorts.DistanceDesc => discounts.OrderByDescending(d => d.DistanceInMeters),
-                _ => discounts.OrderBy(d => d.DistanceInMeters),
-            };
     }
 }
