b8427d2264c607b7fc6269c782cee22582795831
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-09 01:20:56 +0500

    added point of sale service

--- InternshipBe/BL/Interfaces/IPointOfSaleService.cs
+++ InternshipBe/BL/Interfaces/IPointOfSaleService.cs
@@ -1,12 +1,11 @@
-﻿using System;
+﻿using BL.DTO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BL.Interfaces
 {
     public interface IPointOfSaleService
     {
+        Task<IEnumerable<PointOfSaleDTO>> GetPointOfSales();
     }
 }
---
+++ InternshipBe/BL/Services/PointOfSaleService.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BL.DTO;
+using BL.Interfaces;
+using DAL.Entities;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Services
+{
+    public class PointOfSaleService : IPointOfSaleService
+    {
+        private readonly IRepository<PointOfSale> _pointOfSaleRepository;
+        private readonly IMapper _mapper;
+        public PointOfSaleService(IRepository<PointOfSale> pointOfSaleRepository, IMapper mapper)
+        {
+            _pointOfSaleRepository = pointOfSaleRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<PointOfSaleDTO>> GetPointOfSales()
+        {
+            var pointOfdSales = await _pointOfSaleRepository.GetAllAsync();
+
+            return _mapper.Map<PointOfSaleDTO[]>(pointOfdSales);
+        }
+    }
+}
