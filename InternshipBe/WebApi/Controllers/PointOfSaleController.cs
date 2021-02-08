8b8a9370ee18288250f984211c3fa5ae6bb3b7bd
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-09 01:03:44 +0500

    added point of sale controller

---
+++ InternshipBe/WebApi/Controllers/PointOfSaleController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [Route("api/pointOfSales")]
+    [Authorize]
+    public class PointOfSaleController : ControllerBase
+    {
+        private readonly IPointOfSaleService _pointOfSaleService;
+        public PointOfSaleController(IPointOfSaleService pointOfSaleService)
+        {
+            _pointOfSaleService = pointOfSaleService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPointOfSales()
+        {
+            return Ok(await _pointOfSaleService.GetPointOfSales());
+        }
+    }
+}
