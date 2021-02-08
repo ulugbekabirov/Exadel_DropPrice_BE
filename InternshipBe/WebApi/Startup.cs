e20fc9ff74afcedddabd11faf8f5aac8e8f5bc94
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-09 00:11:58 +0500

    added auth in main web api

--- InternshipBe/WebApi/Startup.cs
+++ InternshipBe/WebApi/Startup.cs
@@ -38,8 +38,40 @@ namespace WebApi
 
             services.AddCors();
 
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc(name: "v1", new OpenApiInfo { Title = "Web API" });
+            services.AddSwaggerGen(swagger =>
+            {
+                //This is to generate the Default UI of Swagger Documentation  
+                swagger.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "JWT Token Authentication API",
+                    Description = "Web API"
+                });
+                // To Enable authorization using Swagger (JWT)  
+                swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+                swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+
+                    }
+                });
             });
 
             services.AddAutoMapper(c => c.AddProfile<MappingProfile>(), typeof(Startup));
