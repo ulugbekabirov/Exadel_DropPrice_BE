53f44eee81a2cb168a22286227a05e30ae84b5c8
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-09 01:21:08 +0500

    added point of sale contoller

--- InternshipBe/WebApi/Controllers/PointOfSaleController.cs
+++ InternshipBe/WebApi/Controllers/PointOfSaleController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BL.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
