2dc8030ec86e82624472504c2c3328ba693d7d44
= <romanstat@mail.ru>
2021-02-09 14:02:55 +0300

    Added PointOfSaleViewModel

---
+++ InternshipBe/WebApi/ViewModels/PointOfSaleViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApi.ViewModels
+{
+    public class PointOfSaleViewModel
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public double Latitude { get; set; }
+
+        public double Longitude { get; set; }
+    }
+}
