623bae5745393b4a9d7b3b759a56cd4ff86cf43e
= <romanstat@mail.ru>
2021-02-09 14:03:05 +0300

    Added DiscountViewModel

--- InternshipBe/WebApi/ViewModels/DiscountViewModel.cs
+++ InternshipBe/WebApi/ViewModels/DiscountViewModel.cs
@@ -1,6 +1,28 @@
-﻿namespace WebApi.ViewModels
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApi.ViewModels
 {
     public class DiscountViewModel
     {
+        public string VendorName { get; set; }
+
+        public string DiscountName { get; set; }
+
+        public string Description { get; set; }
+
+        public int DiscountAmount { get; set; }
+
+        public string PromoCode { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public bool ActivityStatus { get; set; }
+
+        public string[] Tags { get; set; }
+
+        public PointOfSaleViewModel[] PointOfSales { get; set; }
     }
 }
