263964219cac0fe2be0e3b7b1529561fbdf979a8
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-10 17:25:05 +0500

    created view model

--- InternshipBe/Shared/ViewModels/VendorViewModel.cs
+++ InternshipBe/Shared/ViewModels/VendorViewModel.cs
@@ -2,5 +2,26 @@
 {
     public class VendorViewModel
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string Phone { get; set; }
+
+        public string FacebookLink { get; set; }
+
+        public string InstagramLink { get; set; }
+
+        public string VkLink { get; set; }
+
+        public string TwitterLink { get; set; }
+
+        public string WebSiteLink { get; set; }
     }
 }
