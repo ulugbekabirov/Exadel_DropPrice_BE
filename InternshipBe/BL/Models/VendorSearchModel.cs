8c56063c1b2aa0781c7495916a91dc9d65727fa3
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-11 19:53:22 +0500

    vendor search model

--- InternshipBe/BL/Models/VendorSearchModel.cs
+++ InternshipBe/BL/Models/VendorSearchModel.cs
@@ -1,6 +1,11 @@
 ﻿namespace BL.Models
 {
-    class VendorSearchModel
+    public class VendorSearchModel : SpecifiedAmountModel
     {
+        public string SearchQuery { get; set; }
+
+        public string RatingSort { get; set; }
+
+        public string TicketCountSort { get; set; }
     }
 }
