4684d6fc68a28e79fdd853cf6412f1c474bea28c
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-11 23:50:47 +0500

    moved switch to extensions

--- InternshipBe/BL/Extensions/VendorExtension.cs
+++ InternshipBe/BL/Extensions/VendorExtension.cs
@@ -1,6 +1,7 @@
 ﻿using BL.DTO;
 using BL.Extensions;
 using DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -17,5 +18,25 @@ namespace Shared.Extensions
         {
             return vendor.Discounts.Sum(d => d.Tickets.Count);
         }
+
+        public static IOrderedEnumerable<VendorDTO> SortBy(this IEnumerable<VendorDTO> vendors, string sortBy)
+        => sortBy switch
+        {
+            "RatingAsc" => vendors.OrderBy(v => v.VendorRating),
+            "RatingDesc" => vendors.OrderByDescending(v => v.VendorRating),
+            "TicketCountAsc" => vendors.OrderBy(v => v.TicketCount),
+            "TicketCountDesc" => vendors.OrderByDescending(v => v.TicketCount),
+            _ => throw new NotImplementedException(),
+        };
+
+        public static IOrderedEnumerable<VendorDTO> ThenSortBy(this IOrderedEnumerable<VendorDTO> vendors, string sortBy)
+        => sortBy switch
+        {
+            "RatingAsc" => vendors.ThenBy(v => v.VendorRating),
+            "RatingDesc" => vendors.ThenByDescending(v => v.VendorRating),
+            "TicketCountAsc" => vendors.ThenBy(v => v.TicketCount),
+            "TicketCountDesc" => vendors.ThenByDescending(v => v.TicketCount),
+            _ => throw new NotImplementedException(),
+        };
     }
 }
