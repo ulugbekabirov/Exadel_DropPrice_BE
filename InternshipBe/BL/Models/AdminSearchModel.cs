db953094e7a6c284c0f6a7f9584fe1da62d502e7
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-11 20:38:20 +0500

    renamed model and properties

---
+++ InternshipBe/BL/Models/AdminSearchModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BL.Models
+{
+    public class AdminSearchModel : SpecifiedAmountModel
+    {
+        public string SearchQuery { get; set; }
+
+        public bool SortByRatingAsc { get; set; }
+
+        public bool SortByTicketCountAsc { get; set; }
+    }
+}
