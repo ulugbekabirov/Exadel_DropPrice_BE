a31f382397bc6ec6707542c26a5d66823e84c3ea
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-11 22:51:18 +0500

    added search

---
+++ InternshipBe/DAL/Interfaces/IVendorRepository.cs
@@ -0,0 +1,10 @@
+﻿using DAL.Entities;
+using System.Linq;
+
+namespace DAL.Interfaces
+{
+    public interface IVendorRepository : IRepository<Vendor>
+    {
+        IQueryable<Vendor> SearchVendors(string searchQuery);
+    }
+}
---
+++ InternshipBe/DAL/Repositories/VendorRepository.cs
@@ -0,0 +1,20 @@
+﻿using DAL.DataContext;
+using DAL.Entities;
+using DAL.Interfaces;
+using System.Linq;
+
+namespace DAL.Repositories
+{
+    public class VendorRepository: Repository<Vendor>, IVendorRepository 
+    {
+        public VendorRepository(ApplicationDbContext context) : base(context)
+        {
+
+        }
+
+        public IQueryable<Vendor> SearchVendors(string searchQuery)
+        {
+            return _context.Vendors.Where(v => v.Name.Contains(searchQuery)).AsQueryable();
+        }
+    }
+}
