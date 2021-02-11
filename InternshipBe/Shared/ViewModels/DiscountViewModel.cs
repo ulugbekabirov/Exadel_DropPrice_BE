4c9ace4ddbe1efb52764d407e4eac983993307b1
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-11 14:02:29 +0500

    added data validation to dvm

--- InternshipBe/Shared/ViewModels/DiscountViewModel.cs
+++ InternshipBe/Shared/ViewModels/DiscountViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApi.ViewModels
 {
@@ -6,20 +7,27 @@ namespace WebApi.ViewModels
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "Please enter the discount's VendorName.")]
         public string VendorName { get; set; }
 
+        [Required(ErrorMessage = "Please enter the discount's Name.")]
         public string DiscountName { get; set; }
 
+        [Required(ErrorMessage = "Please enter the discount's Description.")]
         public string Description { get; set; }
 
+        [Required(ErrorMessage = "Please enter the discount's DiscountAmount.")]
         public int DiscountAmount { get; set; }
 
         public string PromoCode { get; set; }
 
+        [Required(ErrorMessage = "Please enter the discount's StartDate.")]
         public DateTime StartDate { get; set; }
 
+        [Required(ErrorMessage = "Please enter the discount's EndDate.")]
         public DateTime EndDate { get; set; }
 
+        [Required(ErrorMessage = "Please enter the discount's ActivityStatus.")]
         public bool ActivityStatus { get; set; }
 
         public string[] Tags { get; set; }
