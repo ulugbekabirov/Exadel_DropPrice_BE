be36ee75ffcee5534c1433603e79f6f1494b3249
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-13 19:22:49 +0500

    refactoting

--- InternshipBe/Shared/EmailService/EmailSender.cs
+++ InternshipBe/Shared/EmailService/EmailSender.cs
@@ -1,10 +1,6 @@
 ﻿using DAL.Entities;
 using MailKit.Net.Smtp;
 using MimeKit;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Shared.EmailService
