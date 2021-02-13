7ff1cd23b5126e0e8590621ada1cef38d2e50c03
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-13 18:30:43 +0500

    added email sender to ticket repository

--- InternshipBe/BL/Services/TicketService.cs
+++ InternshipBe/BL/Services/TicketService.cs
@@ -3,6 +3,7 @@ using BL.DTO;
 using BL.Interfaces;
 using DAL.Entities;
 using DAL.Interfaces;
+using Shared.EmailService;
 using System.Threading.Tasks;
 
 namespace BL.Services
@@ -11,11 +12,14 @@ namespace BL.Services
     {
         private readonly ITicketRepository _ticketRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailSender _emailSender;
 
-        public TicketService(ITicketRepository repository, IMapper mapper)
+        public TicketService(ITicketRepository repository, IMapper mapper, IEmailSender emailSender)
         {
             _ticketRepository = repository;
             _mapper = mapper;
+            _emailSender = emailSender;
+            
         }
 
         public async Task<TicketDTO> GetOrCreateTicketAsync(int discountId, User user)
@@ -25,6 +29,8 @@ namespace BL.Services
             if (userTicket is null)
             {
                 userTicket = await _ticketRepository.CreateTicketAsync(discountId, user);
+                var message = _emailSender.GenerateMessageTemplate(user, userTicket);
+                _emailSender.SendEmail(message);
                 await _ticketRepository.SaveChangesAsync();
             }
 
