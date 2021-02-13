71da871bdf6338f0683d46639e6c208bdcf07ba9
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-13 18:31:13 +0500

    emlemented send method

--- InternshipBe/Shared/EmailService/EmailSender.cs
+++ InternshipBe/Shared/EmailService/EmailSender.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DAL.Entities;
+using MailKit.Net.Smtp;
+using MimeKit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +12,60 @@ namespace Shared.EmailService
     public class EmailSender : IEmailSender
     {
         private readonly EmailConfiguration _emailConfiguration;
+
+        public EmailSender(EmailConfiguration emailConfiguration)
+        {
+            _emailConfiguration = emailConfiguration;
+        }
+
+        public Message GenerateMessageTemplate(User user, Ticket ticket)
+        {
+            string content = $"This is test message for {user.Email}, {ticket.Discount.Vendor.Name} have given you the ticket as a discount certificate for {ticket.Discount.Name} at {ticket.OrderDate}";
+            return new Message(new string[] { user.Email, ticket.Discount.Vendor.Email }, $"Discount for {ticket.Discount.Name}", content);
+        }
+
         public void SendEmail(Message message)
         {
-            throw new NotImplementedException();
+            var emailMessage = CreateEmailMessage(message);
+
+            Send(emailMessage);
+        }
+
+        private MimeMessage CreateEmailMessage(Message message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_emailConfiguration.From));
+            emailMessage.To.AddRange(message.To);
+            emailMessage.Subject = message.Subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+            return emailMessage;
+        }
+
+        private void Send(MimeMessage message)
+        {
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    client.Connect(_emailConfiguration.SmtpServer, _emailConfiguration.Port, true);
+                    client.AuthenticationMechanisms.Remove("XOAUTH2");
+                    client.Authenticate(_emailConfiguration.Username, _emailConfiguration.Password);
+
+                    client.Send(message);
+                }
+                catch 
+                {
+
+                    throw;
+                }
+                finally
+                {
+                    client.Disconnect(true);
+                    client.Dispose();
+                }
+
+            }
         }
     }
 }
--- InternshipBe/Shared/EmailService/IEmailSender.cs
+++ InternshipBe/Shared/EmailService/IEmailSender.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,6 @@ namespace Shared.EmailService
     public interface IEmailSender
     {
         void SendEmail(Message message);
+        Message GenerateMessageTemplate(User user, Ticket ticket);
     }
 }
