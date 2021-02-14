9aa61e7477f5e794a48fa2a819cae1b2ede4904f
Андрей Драгун <apocaliptik450@gmail.com>
2021-02-14 07:58:32 +0300

    implimented bedta version of HintsService

---
+++ InternshipBe/BL/Services/HintsService.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using BL.Interfaces;
+using BL.Models;
+using DAL.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BL.Services
+{
+    public class HintsService : IHintsService
+    {
+        private readonly IHintsRepository _searchRepository;
+        private readonly IMapper _mapper;
+
+        public HintsService(IHintsRepository repository, IMapper mapper)
+        {
+            _searchRepository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<string>> HintsAsync(string subString, SpecifiedAmountModel specifiedAmountModel)
+        {
+            var hints =  await _searchRepository.SearchHintsAsync(subString, specifiedAmountModel.Take);
+
+            return (hints);
+        }
+    }
+}
---
+++ InternshipBe/DAL/Interfaces/IHintsRepository.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IHintsRepository : IRepository<Discount>
+    {
+        Task<IEnumerable<string>> SearchHintsAsync(string subString, int take);
+    }
+}
