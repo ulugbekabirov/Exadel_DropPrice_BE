b1bee73d8c08e27c4c94bfd0ea874d1b513d6b17
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-14 16:23:25 +0500

    removed ctor

--- InternshipBe/Shared/EmailService/Message.cs
+++ InternshipBe/Shared/EmailService/Message.cs
@@ -11,13 +11,5 @@ namespace Shared.EmailService
         public string Subject { get; set; }
 
         public string Content { get; set; }
-
-        public Message(IEnumerable<string> to, string subject, string content)
-        {
-            To = new List<MailboxAddress>();
-            To.AddRange(to.Select(x => new MailboxAddress(x)));
-            Subject = subject;
-            Content = content;
-        }
     }
 }
