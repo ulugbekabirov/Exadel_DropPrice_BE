2f5a40f3001a7c922e08be976a00837802ef0efe
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-14 16:23:50 +0500

    reimplemented generate method

--- InternshipBe/Shared/EmailService/EmailSender.cs
+++ InternshipBe/Shared/EmailService/EmailSender.cs
@@ -1,6 +1,7 @@
 ﻿using DAL.Entities;
 using MailKit.Net.Smtp;
 using MimeKit;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace Shared.EmailService
@@ -17,36 +18,46 @@ namespace Shared.EmailService
         public Message GenerateMessageTemplate(User user, Ticket ticket)
         {
             var content = $"This is test message for {user.Email}, {ticket.Discount.Vendor.Name} have given you the ticket as a discount certificate for {ticket.Discount.Name} at {ticket.OrderDate}";
-            return new Message(new string[] { user.Email, ticket.Discount.Vendor.Email }, $"Discount for {ticket.Discount.Name}", content);
-        }
 
-        public async Task SendEmailAsync(Message message)
-        {
-            var emailMessage = CreateEmailMessage(message);
+            var subject = $"Discount for {ticket.Discount.Name}";
 
-            await SendAsync(emailMessage);
-        }
-
-        private MimeMessage CreateEmailMessage(Message message)
-        {
-            var emailMessage = new MimeMessage();
-            emailMessage.From.Add(new MailboxAddress(_emailConfiguration.From));
-            emailMessage.To.AddRange(message.To);
-            emailMessage.Subject = message.Subject;
-            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
-
-            return emailMessage;
-        }
-
-        private async Task SendAsync(MimeMessage message)
-        {
-            using (var client = new SmtpClient())
+            var message = new Message()
             {
-                await client.ConnectAsync(_emailConfiguration.SmtpServer, _emailConfiguration.Port, true);
-                client.AuthenticationMechanisms.Remove("XOAUTH2");
-                await client.AuthenticateAsync(_emailConfiguration.Username, _emailConfiguration.Password);
-                await client.SendAsync(message);
-            }
+                To = new List<MailboxAddress>() {new MailboxAddress(user.Email),new MailboxAddress(ticket.Discount.Vendor.Email)},
+                Subject = subject,
+                Content = content,
+
+            };
+            return message;
+        }
+
+    public async Task SendEmailAsync(Message message)
+    {
+        var emailMessage = CreateEmailMessage(message);
+
+        await SendAsync(emailMessage);
+    }
+
+    private MimeMessage CreateEmailMessage(Message message)
+    {
+        var emailMessage = new MimeMessage();
+        emailMessage.From.Add(new MailboxAddress(_emailConfiguration.From));
+        emailMessage.To.AddRange(message.To);
+        emailMessage.Subject = message.Subject;
+        emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Text) { Text = message.Content };
+
+        return emailMessage;
+    }
+
+    private async Task SendAsync(MimeMessage message)
+    {
+        using (var client = new SmtpClient())
+        {
+            await client.ConnectAsync(_emailConfiguration.SmtpServer, _emailConfiguration.Port, true);
+            client.AuthenticationMechanisms.Remove("XOAUTH2");
+            await client.AuthenticateAsync(_emailConfiguration.Username, _emailConfiguration.Password);
+            await client.SendAsync(message);
         }
     }
+}
 }
