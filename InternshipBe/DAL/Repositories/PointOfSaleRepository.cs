963996e9ea0228428da45fe06b827bacf467a7e8
= <romanstat@mail.ru>
2021-02-16 15:26:47 +0300

    Added PointOfSale repository

---
+++ InternshipBe/DAL/Repositories/PointOfSaleRepository.cs
@@ -0,0 +1,22 @@
+﻿using DAL.DataContext;
+using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class PointOfSaleRepository : Repository<PointOfSale>, IPointOfSaleRepository
+    {
+        public PointOfSaleRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<PointOfSale>> GetExistingPointOfSalesAsync(IEnumerable<string> pointOfSalesName)
+        {
+            return await _context.PointOfSales.Where(p => pointOfSalesName.Contains(p.Name)).ToListAsync();
+        }
+    }
+}
