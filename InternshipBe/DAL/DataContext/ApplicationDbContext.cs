bf11f0eec8a789b5845a3646990b7e24a56ea59c
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-17 21:30:28 +0500

    added db set of images

--- InternshipBe/DAL/DataContext/ApplicationDbContext.cs
+++ InternshipBe/DAL/DataContext/ApplicationDbContext.cs
@@ -33,6 +33,8 @@ namespace DAL.DataContext
 
         public DbSet<ConfigVariable> ConfigVariables { get; set; }
 
+        public DbSet<Image> Images { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
