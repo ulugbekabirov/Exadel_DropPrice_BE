29096f1c9f496b3870753be0b3d7dc7f281160f6
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-18 01:06:46 +0500

    added basic services

---
+++ InternshipBe/BL/Interfaces/IImageService.cs
@@ -0,0 +1,7 @@
+﻿namespace BL.Interfaces
+{
+    public interface IImageService
+    {
+        
+    }
+}
---
+++ InternshipBe/BL/Services/ImageService.cs
@@ -0,0 +1,18 @@
+﻿using BL.Interfaces;
+using DAL.Interfaces;
+
+namespace BL.Services
+{
+    public class ImageService: IImageService
+    {
+        private readonly IImageRepository _imageRepository;
+
+        public ImageService(IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+        }
+
+        
+
+    }
+}
