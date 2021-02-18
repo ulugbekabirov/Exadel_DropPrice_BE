cb0d3c89c6cf24b48d0bb571d3af4d5fddc35772
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-18 21:15:58 +0500

    bug fix

--- InternshipBe/DAL/Interfaces/IImageRepository.cs
+++ InternshipBe/DAL/Interfaces/IImageRepository.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace DAL.Interfaces
 {
-    public interface IImageRepository
+    public interface IImageRepository:IRepository<Image>
     {
         Task<Image> CreateAndReturnImageAsync(byte[] imageData, string filename);
     }
