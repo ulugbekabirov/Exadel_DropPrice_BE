71d1175ac4dd31561821beed0e73e837b8546815
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-18 21:15:23 +0500

    added get image method

--- InternshipBe/BL/Interfaces/IImageService.cs
+++ InternshipBe/BL/Interfaces/IImageService.cs
@@ -1,10 +1,12 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DAL.Entities;
+using Microsoft.AspNetCore.Http;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace BL.Interfaces
 {
     public interface IImageService
     {
-        Task AddImageToVendor(IFormFile file, int vendorId);
+        Task<string> GetImageByIdAsync(int imageId);
     }
 }
