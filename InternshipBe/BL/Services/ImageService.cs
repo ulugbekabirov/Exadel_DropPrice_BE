8557bd6499f83ef3769d8aa3b2b0ee1286314aed
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-18 21:15:47 +0500

    implemented method

--- InternshipBe/BL/Services/ImageService.cs
+++ InternshipBe/BL/Services/ImageService.cs
@@ -3,6 +3,7 @@ using BL.Interfaces;
 using DAL.Entities;
 using DAL.Interfaces;
 using Microsoft.AspNetCore.Http;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -12,13 +13,18 @@ namespace BL.Services
     public class ImageService : IImageService
     {
         private readonly IImageRepository _imageRepository;
-        private readonly IVendorRepository _vendorRepository;
 
-        public ImageService(IImageRepository imageRepository, IVendorRepository vendorRepository)
+        public ImageService(IImageRepository imageRepository)
         {
             _imageRepository = imageRepository;
-            _vendorRepository = vendorRepository;
         }
 
+        public async Task<string> GetImageByIdAsync(int imageId)
+        {
+            var image = await _imageRepository.GetByIdAsync(imageId);
+            var imageBase64Data = Convert.ToBase64String(image.ImageData);
+            var imageDataURL = string.Format("data:image/png;base64,{0}", imageBase64Data);
+            return imageDataURL;
+        }
     }
 }
