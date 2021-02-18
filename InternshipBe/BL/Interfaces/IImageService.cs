a47bb8ca10476cec4bf7aa7f18d389afc927b945
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-18 17:49:04 +0500

    added image service

--- InternshipBe/BL/Interfaces/IImageService.cs
+++ InternshipBe/BL/Interfaces/IImageService.cs
@@ -1,7 +1,10 @@
-﻿namespace BL.Interfaces
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace BL.Interfaces
 {
     public interface IImageService
     {
-        
+        Task AddImageToVendor(IFormFile file, int vendorId);
     }
 }
--- InternshipBe/BL/Services/ImageService.cs
+++ InternshipBe/BL/Services/ImageService.cs
@@ -1,18 +1,24 @@
-﻿using BL.Interfaces;
+﻿using BL.DTO;
+using BL.Interfaces;
+using DAL.Entities;
 using DAL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BL.Services
 {
-    public class ImageService: IImageService
+    public class ImageService : IImageService
     {
         private readonly IImageRepository _imageRepository;
+        private readonly IVendorRepository _vendorRepository;
 
-        public ImageService(IImageRepository imageRepository)
+        public ImageService(IImageRepository imageRepository, IVendorRepository vendorRepository)
         {
             _imageRepository = imageRepository;
+            _vendorRepository = vendorRepository;
         }
-
-        
 
     }
 }
