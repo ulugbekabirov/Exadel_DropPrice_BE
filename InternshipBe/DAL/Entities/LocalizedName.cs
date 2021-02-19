4eef93d425e8c4a3ca37ec8ad374ef0f804e3f75
= <romanstat@mail.ru>
2021-02-19 22:06:09 +0300

    Added LocalizedName

---
+++ InternshipBe/DAL/Entities/LocalizedName.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Threading;
+
+namespace DAL.Entities
+{
+    [ComplexType]
+    public class LocalizedName
+    {
+        public int Id { get; set; }
+
+        public string Russian { get; set; }
+
+        public string English { get; set; }
+
+        [NotMapped]
+        public string Current
+        {
+            get
+            {
+                return (Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName.ToUpper()) switch
+                {
+                    "RU" => Russian,
+                    "EN" => English,
+                    _ => English,
+                };
+            }
+        }
+    }
+}
