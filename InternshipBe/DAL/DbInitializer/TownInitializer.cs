830936289eb815f1b641a7b2e17457aa89feff37
= <romanstat@mail.ru>
2021-02-19 22:08:18 +0300

    Updated TownInitiazlier

--- InternshipBe/DAL/DbInitializer/TownInitializer.cs
+++ InternshipBe/DAL/DbInitializer/TownInitializer.cs
@@ -14,22 +14,30 @@ namespace DAL.DbInitializer
 
         public void InitializeTowns()
         {
-            AddTown("Минск", 53.9005961, 27.5589895);
-            AddTown("Гомель", 52.4313156, 30.9938049);
-            AddTown("Ташкент", 41.2994958, 69.2400734);
-            AddTown("Walnut Creek", 37.9100754, -122.0653152);
-            AddTown("Варшава", 52.231164, 21.0113525); 
-            AddTown("Витебск", 55.193419, 30.2038716);
+            AddTown("Минск", "Minsk", 53.9005961, 27.5589895);
+            AddTown("Гомель", "Gomel", 52.4313156, 30.9938049);
+            AddTown("Ташкент", "Tashkent", 41.2994958, 69.2400734);
+            AddTown("Уолнат - Крик", "Walnut Creek", 37.9100754, -122.0653152);
+            AddTown("Варшава", "Warszawa", 52.231164, 21.0113525); 
+            AddTown("Витебск", "Vitebsk", 55.193419, 30.2038716);
         }
 
-        public void AddTown(string name, double latitude, double longitude)
+        public void AddTown(string nameRussian, string NameEnglish, double latitude, double longitude)
         {
-            _context.Towns.Add(new Town()
+            var localizedName = new LocalizedName()
             {
-                Name = name,
+                Russian = nameRussian,
+                English = NameEnglish,
+            };
+
+            var town = new Town()
+            {
+                Name = localizedName,
                 Latitude = latitude,
                 Longitude = longitude,
-            });
+            };
+
+            _context.Towns.Add(town);
 
             _context.SaveChanges();
         }
