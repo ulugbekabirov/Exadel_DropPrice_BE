30d43dceb0811925029328991512a3c17e89ef3c
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-20 14:31:12 +0500

    added culture to thread

--- InternshipBe/Shared/Localization/CultureMiddleware.cs
+++ InternshipBe/Shared/Localization/CultureMiddleware.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Shared.Localization
@@ -22,13 +23,13 @@ namespace Shared.Localization
             var lang = context.Request.Headers["Accept-Language"].ToString();
             if (!string.IsNullOrWhiteSpace(lang))
             {
-                CultureInfo.CurrentCulture = new CultureInfo(lang);
-                CultureInfo.CurrentUICulture = new CultureInfo(lang);
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(lang);
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(lang);
             }
             else
             {
-                CultureInfo.CurrentCulture = new CultureInfo("ru");
-                CultureInfo.CurrentUICulture = new CultureInfo("ru");
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("ru");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("ru");
             }
 
             await _next.Invoke(context);
