053e08334fd90435878bf08d72d4dc2a0c652c3c
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-20 14:53:37 +0500

    added  culture middleware extentions

---
+++ InternshipBe/Shared/Localization/CultureMiddlewareExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Localization
+{
+    public static class CultureMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCulture(
+            this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CultureMiddleware>();
+        }
+    }
+}
