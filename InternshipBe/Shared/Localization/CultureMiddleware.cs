79c5a9ee191451f052677eba5e619f15d0b79911
ulugbekabirov <ulugbekabirov@gmail.com>
2021-02-20 14:22:31 +0500

    added culture middlewate

---
+++ InternshipBe/Shared/Localization/CultureMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Localization
+{
+    public class CultureMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CultureMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var lang = context.Request.Headers["Accept-Language"].ToString();
+            if (!string.IsNullOrWhiteSpace(lang))
+            {
+                CultureInfo.CurrentCulture = new CultureInfo(lang);
+                CultureInfo.CurrentUICulture = new CultureInfo(lang);
+            }
+            else
+            {
+                CultureInfo.CurrentCulture = new CultureInfo("ru");
+                CultureInfo.CurrentUICulture = new CultureInfo("ru");
+            }
+
+            await _next.Invoke(context);
+        }
+    }
+}
