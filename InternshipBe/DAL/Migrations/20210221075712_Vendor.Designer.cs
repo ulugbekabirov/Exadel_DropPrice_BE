b1c2b2a1d7f2b92c1a9ebdcb02f5e379fc29f8f7
= <romanstat@mail.ru>
2021-02-21 21:31:46 +0300

    Add migration

--- InternshipBe/DAL/Migrations/20210221075712_Vendor.Designer.cs
+++ InternshipBe/DAL/Migrations/20210221075712_Vendor.Designer.cs
@@ -4,15 +4,17 @@ using DAL.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210221075712_Vendor")]
+    partial class Vendor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ InternshipBe/DAL/Migrations/20210221075712_Vendor.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class Vendor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PointOfSaleVendor",
+                columns: table => new
+                {
+                    PointOfSalesId = table.Column<int>(type: "int", nullable: false),
+                    VendorsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PointOfSaleVendor", x => new { x.PointOfSalesId, x.VendorsId });
+                    table.ForeignKey(
+                        name: "FK_PointOfSaleVendor_PointOfSales_PointOfSalesId",
+                        column: x => x.PointOfSalesId,
+                        principalTable: "PointOfSales",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PointOfSaleVendor_Vendors_VendorsId",
+                        column: x => x.VendorsId,
+                        principalTable: "Vendors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PointOfSaleVendor_VendorsId",
+                table: "PointOfSaleVendor",
+                column: "VendorsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PointOfSaleVendor");
+        }
+    }
+}
