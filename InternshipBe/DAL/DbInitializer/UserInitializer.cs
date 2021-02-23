73b2d251803b7fdd0ae02a2451373e587c0eb384
= <romanstat@mail.ru>
2021-02-23 19:16:22 +0300

    added real data to userInitializer

--- InternshipBe/DAL/DbInitializer/UserInitializer.cs
+++ InternshipBe/DAL/DbInitializer/UserInitializer.cs
@@ -20,21 +20,21 @@ namespace DAL.DbInitializer
 
         public async Task InitializeUsers()
         {
-            await AddUser(1, "admnexadel@gmail.com", "admin", "admin", "admin", "+375447777777", true, _context.Offices.Find(1), RoleNames.Admin);
+            await AddUser(1, "admnexadel@gmail.com", "Альберт", "Эйнштейн", "Германович", "+375447777777", true, _context.Offices.Find(1), RoleNames.Admin);
 
-            await AddUser(1, "moderatorexadel@gmail.com", "moderator", "moderator", "moderator", "+375447777777", true, _context.Offices.Find(1), RoleNames.Moderator);
+            await AddUser(1, "moderatorexadel@gmail.com", "Ницше", "Фридрих", "Вильгельм", "+375447777777", true, _context.Offices.Find(1), RoleNames.Moderator);
 
-            await AddUser(1, "userexadel@gmail.com", "demoUser", "demoUser", "demoUser", "+375447777777", true, _context.Offices.Find(1), RoleNames.User);
+            await AddUser(1, "userexadel@gmail.com", "Илон", "Маск", "Рив", "+375447777777", true, _context.Offices.Find(1), RoleNames.User);
 
-            await AddUser(1, "userMinsk@test.com", "офис", "Минск", "Минск", "+375447777777", false, _context.Offices.Find(1), RoleNames.User);
+            await AddUser(1, "userMinsk@test.com", "Хуанг", "Дженсен", "Жэньсюнь", "+375447777777", false, _context.Offices.Find(1), RoleNames.User);
 
-            await AddUser(2, "userGomel@test.com", "офис", "Гомель", "Гомель", "+375447777777", true, _context.Offices.Find(2), RoleNames.User);
+            await AddUser(2, "userGomel@test.com", "Джефф", "Безос", "Престон", "+375447777777", true, _context.Offices.Find(2), RoleNames.User);
 
-            await AddUser(3, "userWarszawa@test.com", "офис", "Варшава", "Варшава", "+48 228 236 200", true, _context.Offices.Find(3), RoleNames.User);
+            await AddUser(3, "userWarszawa@test.com", "Пол", "Аллен", "Гарднер", "+48 228 236 200", true, _context.Offices.Find(3), RoleNames.User);
 
-            await AddUser(4, "userTashkent@test.com", "офис", "Ташкент", "Ташкент", "+78-140-09-09", true, _context.Offices.Find(4), RoleNames.User);
+            await AddUser(4, "userTashkent@test.com", "Билл", "Гейтс", "Уильям", "+78-140-09-09", true, _context.Offices.Find(4), RoleNames.User);
            
-            await AddUser(5, "userUsa@test.com", "офис", "Walnut Creek", "Walnut Creek", "+1 (916) 555 0166", true, _context.Offices.Find(5), RoleNames.User);
+            await AddUser(5, "userUsa@test.com", "Джобс", "Стивен", "Пол", "+1 (916) 555 0166", true, _context.Offices.Find(5), RoleNames.User);
 
             Random random = new Random();
 
