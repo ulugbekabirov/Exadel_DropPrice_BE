01f965bf22d1d4060ec866844eb1313482b9be18
Андрей Драгун <apocaliptik450@gmail.com>
2021-02-25 11:15:16 +0300

    added UnitTest for ReplacerService

---
+++ InternshipBe/UnitTests/EmailService/ReplacerServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using BL.EmailService;
+using System.Collections.Generic;
+using Xunit;
+
+namespace UnitTests.EmailService
+{
+    public class ReplacerServiceTest
+    {
+        [Fact]
+        public void Replacer_TransmittedString_StringModifiedWithRegulars()
+        {
+            //arrange
+            var replacer = new ReplacerService()
+            {
+            };
+            var expecetOutputString = "Albert Einstein Germanovich you received a 33% discount to Nosovhki coupon on 22.01 from a supplier. supplier contact details - Phone:+123321 Email: venor@gmail.com";
+            var inputString = "##FirstName## ##LastName## ##Patronymic## you received a ##DiscountValue##% discount to ##DiscountName## coupon on ##Date## from a supplier. supplier contact details - Phone:##VendorPhone## Email: ##VendorEmail##";
+            var dictionary = new Dictionary<string, string>
+                {
+                    { "FirstName", "Albert" },
+                    { "LastName", "Einstein" },
+                    { "Patronymic", "Germanovich" },
+                    { "Date", "22.01" },
+                    { "DiscountName", "Nosovhki" },
+                    { "DiscountValue", "33" },
+                    { "Vendor", "Belwest" },
+                    { "VendorPhone", "+123321" },
+                    { "VendorEmail", "venor@gmail.com" },
+                    { "Promocode", null }
+                };
+
+            //act
+            var outpuSting = replacer.Replacer(inputString, dictionary);
+
+            //assert
+            Assert.Equal(expecetOutputString, outpuSting);
+        }
+    }
+}
