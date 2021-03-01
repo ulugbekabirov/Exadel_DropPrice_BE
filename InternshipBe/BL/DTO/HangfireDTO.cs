ec0dfe4f2f898e1cc140660c934c0ae2f4dcce01
= <romanstat@mail.ru>
2021-03-01 12:39:56 +0300

    Added HangfireDTO

---
+++ InternshipBe/BL/DTO/HangfireDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BL.DTO
+{
+    public class HangfireDTO
+    {
+        public string Message { get; set; }
+
+        public int EditTime { get; set; }
+
+        public bool IsEdit { get; set; }
+    }
+}
