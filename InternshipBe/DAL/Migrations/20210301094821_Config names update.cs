6a2a11637ae99c507b28f169a61cb4057b0ed707
ulugbekabirov <ulugbekabirov@gmail.com>
2021-03-01 17:42:22 +0500

    added update sql script

--- InternshipBe/DAL/Migrations/20210301094821_Config names update.cs
+++ InternshipBe/DAL/Migrations/20210301094821_Config names update.cs
@@ -6,7 +6,12 @@ namespace DAL.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.Sql(@"UPDATE ConfigVariables SET Name = 'Sending email toggler' Where Name='SendingEmailToggler';
+                                   UPDATE ConfigVariables SET Name = 'Discount edit time in minutes' Where Name='DiscountEditTimeInMinutes';
+                                   UPDATE ConfigVariables SET Name = 'Email body for User in English' Where Name='EnMessageForUser';
+                                   UPDATE ConfigVariables SET Name = 'Email body for Vendor in English' Where Name='EnMessageForVendor';
+                                   UPDATE ConfigVariables SET Name = 'Email body for User in Russian' Where Name='RuMessageForUser';
+                                   UPDATE ConfigVariables SET Name = 'Email body for Vendor in Russian' Where Name='RuMessageForVendor';");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
