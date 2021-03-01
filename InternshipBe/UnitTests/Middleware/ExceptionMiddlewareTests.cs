83ffa6278c61e1e55fd2d5348f970b5b87119b6f
ulugbekabirov <ulugbekabirov@gmail.com>
2021-03-01 18:54:41 +0500

    removed usings

--- InternshipBe/UnitTests/Middleware/ExceptionMiddlewareTests.cs
+++ InternshipBe/UnitTests/Middleware/ExceptionMiddlewareTests.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 using Shared.ExceptionHandling;
 using Microsoft.AspNetCore.Http;
 using System.IO;
-using Newtonsoft.Json;
 using System.Net;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.Extensions.Logging;
