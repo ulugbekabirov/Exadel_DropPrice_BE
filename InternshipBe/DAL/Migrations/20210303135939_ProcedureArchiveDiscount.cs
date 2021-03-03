efc338aebb2a40bd3948db05d0bc0317c9b81782
= <romanstat@mail.ru>
2021-03-03 18:57:39 +0300

    Added migration stored procedure ArchiveInvalidDiscount

---
+++ InternshipBe/DAL/Migrations/20210303135939_ProcedureArchiveDiscount.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class ProcedureArchiveDiscount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"GO
+                                  CREATE PROCEDURE ArchiveInvalidDiscount AS
+                                  UPDATE Discounts SET ActivityStatus = 0 WHERE ActivityStatus = 1 and EndDate < GETDATE();");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP PROCEDURE ArchiveInvalidDiscount;");
+        }
+    }
+}
