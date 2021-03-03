e5c54c780a7b27adbe334b6911afac341c879af1
= <romanstat@mail.ru>
2021-03-03 18:57:58 +0300

    Added dapper repository with ArchiveInvalidDiscount method

---
+++ InternshipBe/DAL/Repositories/DapperRepository.cs
@@ -0,0 +1,24 @@
+﻿using DAL.Interfaces;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+
+namespace DAL.Repositories
+{
+    public class DapperRepository : IDapperRepository
+    {
+        private readonly string _connectionString;
+
+        public DapperRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public void ArchiveInvalidDiscount()
+        {
+            using IDbConnection context = new SqlConnection(_connectionString);
+            context.Execute("EXEC ArchiveInvalidDiscount");
+        }
+    }
+}
