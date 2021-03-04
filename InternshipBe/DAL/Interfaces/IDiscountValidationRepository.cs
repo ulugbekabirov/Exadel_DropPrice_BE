8d277ac996464565e23d9a781ebc05de48b8bcb5
= <romanstat@mail.ru>
2021-03-04 14:29:50 +0300

    added discountvalidation repository

---
+++ InternshipBe/DAL/Interfaces/IDiscountValidationRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IDiscountValidationRepository
+    {
+        Task CheckDiscountDateAsync(int id);
+    }
+}
---
+++ InternshipBe/DAL/Repositories/DiscountValidationRepository.cs
@@ -0,0 +1,31 @@
+﻿using DAL.DataContext;
+using DAL.Entities;
+using DAL.Interfaces;
+using Microsoft.Extensions.Localization;
+using Shared.Resources;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class DiscountValidationRepository : Repository<Discount>, IDiscountValidationRepository
+    {
+        private readonly IStringLocalizer<NotificationResource> _stringLocalizer;
+
+        public DiscountValidationRepository(ApplicationDbContext context, IStringLocalizer<NotificationResource> stringLocalizer) : base(context)
+        {
+            _stringLocalizer = stringLocalizer;
+        }
+
+        public async Task CheckDiscountDateAsync(int id)
+        {
+            var discount = await GetByIdAsync(id);
+
+            if (discount.StartDate > DateTime.Now)
+            {
+                throw new ValidationException(_stringLocalizer["Discount is not started yet"]);
+            }
+        }
+    }
+}
