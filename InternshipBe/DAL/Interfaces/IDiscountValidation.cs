69a0bdbc71985109502249cc513871a46bb515c1
= <romanstat@mail.ru>
2021-03-04 19:01:15 +0300

    renamed validation

--- InternshipBe/DAL/Interfaces/IDiscountValidation.cs
+++ InternshipBe/DAL/Interfaces/IDiscountValidation.cs
@@ -4,6 +4,6 @@ namespace DAL.Interfaces
 {
     public interface IDiscountValidation
     {
-        Task CheckDiscountStartDateAsync(int id);
+        Task CheckDiscountValidationAsync(int id);
     }
 }
