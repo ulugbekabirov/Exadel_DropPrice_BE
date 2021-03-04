3f00f1d381948aa5c5e880ececdd0d246ca8ac0a
= <romanstat@mail.ru>
2021-03-04 19:46:00 +0300

    added Validator

---
+++ InternshipBe/DAL/Interfaces/IValidator.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    interface IValidator<TEntity> where TEntity : class
+    {
+        Task ValidateAsync(TEntity entity);
+    }
+}
---
+++ InternshipBe/DAL/Repositories/Validator.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class Validator<TEntity> : IValidator<TEntity> where TEntity : class
+    {
+        public async Task ValidateAsync(TEntity entity)
+        {
+            throw new ValidationException($"ValidationException of {entity.GetType()}");
+        }
+    }
+}
