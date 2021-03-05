99af21381fd636ffbdeeb94d82515b07860e73aa
Андрей Драгун <apocaliptik450@gmail.com>
2021-03-05 08:57:42 +0300

    added first test

---
+++ InternshipBe/UnitTests/AutometedTests/LoginTest.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests.AutometedTests
+{
+    class LoginTest
+    {
+    }
+
+    public class AutomatedUITests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+        public AutomatedUITests()
+        {
+            _driver = new ChromeDriver();
+        }
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void Create_WhenExecuted_ReturnsCreateView()
+        {
+            _driver.Navigate()
+                .GoToUrl("https://localhost:44360/api/login");
+            Assert.Equal("", _driver.Title);
+            Assert.Contains("", _driver.PageSource);
+        }
+
+    }
+
+}
