4bf6fce34f4f8548739f7353710530ac979c873a
ulugbekabirov <ulugbekabirov@gmail.com>
2021-03-06 10:04:39 +0500

    refactoring

--- InternshipBe/DAL/Repositories/VendorRepository.cs
+++ InternshipBe/DAL/Repositories/VendorRepository.cs
@@ -16,7 +16,7 @@ namespace DAL.Repositories
 
         public async Task<IEnumerable<PointOfSale>> GetPointOfSalesAsync(int id)
         {
-            var vendor = await _context.Vendors.FirstAsync(v => v.Id == id);
+            var vendor = await GetByIdAsync(id);
 
             return vendor.PointOfSales;
         }
