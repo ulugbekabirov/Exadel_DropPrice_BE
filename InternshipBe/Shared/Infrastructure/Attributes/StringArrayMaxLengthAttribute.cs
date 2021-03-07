998b9870e6dd98e3ae643f0dd59629f4b1128c01
ulugbekabirov <ulugbekabirov@gmail.com>
2021-03-07 12:15:38 +0500

    added custom attribute

---
+++ InternshipBe/Shared/Infrastructure/Attributes/StringArrayMaxLengthAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Infrastructure.Attributes
+{
+    public class StringArrayMaxLengthAttribute : MaxLengthAttribute
+    {
+        public StringArrayMaxLengthAttribute(int Length)
+            :base(Length)
+        {
+        }
+
+        public override bool IsValid(object value)
+        {
+            if (value is not List<string>)
+                return false;
+
+            foreach (var str in value as List<string>)
+            {
+                if (str.Length > Length)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
