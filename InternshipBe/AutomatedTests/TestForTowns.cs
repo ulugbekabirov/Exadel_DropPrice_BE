23362b07eb5f1d9c412e70e913ab81709eed91f1
Андрей Драгун <apocaliptik450@gmail.com>
2021-03-09 09:50:43 +0300

    initial commit for automatedTests

---
+++ InternshipBe/AutomatedTests/TestForTowns.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+
+namespace AutomatedTests
+{
+    public class TestForTowns
+    {
+
+        var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
+
+        IConfiguration config = builder.Build();
+
+    }
+}
