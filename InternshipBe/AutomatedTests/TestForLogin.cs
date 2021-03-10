f52292b99eb86bd37de6d4b00d373081cfda2c3d
Андрей Драгун <apocaliptik450@gmail.com>
2021-03-10 09:27:24 +0300

    impliment tests for login and towns endpoints

---
+++ InternshipBe/AutomatedTests/TestForLogin.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Reflection;
+
+namespace AutomatedTests
+{
+    public class TestForLogin
+    {
+        static readonly HttpClient client = new HttpClient();
+
+        [Fact]
+        public async Task Login_TransitionToEndPoint_GettingToken()
+        {
+            //arrange
+            var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location.Substring(0, Assembly.GetEntryAssembly().Location.IndexOf("bin\\")));
+
+            var config = new ConfigurationBuilder()
+                .SetBasePath(path)
+                .AddJsonFile("appsettings.json")
+                .Build(); ;
+
+            //client.BaseAddress = new Uri("https://dropprice.click:8443/");
+            client.BaseAddress = new Uri(config.GetSection("AppSettings").GetSection("BaseAddress").Value);
+
+            var request = new HttpRequestMessage();
+            var content = request.Content = new StringContent("{\"Email\" : \"admnexadel@gmail.com\", \"Password\" : \"Qwerty123!\"}", Encoding.UTF8, "application/json");
+
+            //act
+            var response = await client.PostAsync("login", content);
+            string result = response.Content.ReadAsStringAsync().Result;
+
+            //assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains("token", result);
+        }
+    }
+}
--- InternshipBe/AutomatedTests/TestForTowns.cs
+++ InternshipBe/AutomatedTests/TestForTowns.cs
@@ -1,18 +1,56 @@
 ﻿using Microsoft.Extensions.Configuration;
 using System;
 using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using System.Reflection;
 
 namespace AutomatedTests
 {
     public class TestForTowns
     {
+        static readonly HttpClient client = new HttpClient();
 
-        var builder = new ConfigurationBuilder()
-    .SetBasePath(Directory.GetCurrentDirectory())
-    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
+        [Fact]
+        public async Task Towns_TransitionToEndPoint_GettingListOfTowns()
+        {
+            //arrange
+            var path = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location.Substring(0, Assembly.GetEntryAssembly().Location.IndexOf("bin\\")));
 
-        IConfiguration config = builder.Build();
+            var config = new ConfigurationBuilder()
+                .SetBasePath(path)
+                .AddJsonFile("appsettings.json")
+                .Build();
 
+            var baseAdrress = config.GetSection("AppSettings").GetSection("BaseAddress").Value;
+
+            var requestForLogin = new HttpRequestMessage();
+            var requestContent = requestForLogin.Content = new StringContent("{\"Email\" : \"admnexadel@gmail.com\", \"Password\" : \"Qwerty123!\"}", Encoding.UTF8, "application/json");
+
+            var responseFromlogin = await client.PostAsync(baseAdrress + "login", requestContent);
+            string token = responseFromlogin.Content.ReadAsStringAsync().Result;
+
+            token = token.Substring(0, token.Length - 2).Remove(0, 10);
+
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(baseAdrress + "towns"),
+                Headers = {
+                    { HttpRequestHeader.Authorization.ToString(), "bearer " + token },
+                },
+            };
+
+            //act
+            var response = await client.SendAsync(httpRequestMessage);
+            string result = response.Content.ReadAsStringAsync().Result;
+
+            //assert
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+            Assert.Contains("townName", result);
+        }
     }
 }
