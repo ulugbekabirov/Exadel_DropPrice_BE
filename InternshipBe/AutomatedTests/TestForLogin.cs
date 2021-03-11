79097b9ba96109fc0517ee81d81661d25310d9be
Андрей Драгун <apocaliptik450@gmail.com>
2021-03-11 17:55:27 +0300

    return old path

--- InternshipBe/AutomatedTests/TestForLogin.cs
+++ InternshipBe/AutomatedTests/TestForLogin.cs
@@ -18,7 +18,8 @@ namespace AutomatedTests
             //arrange
             using var client = new HttpClient();
 
-            var path = AppContext.BaseDirectory.Replace(@"\bin\Debug\net5.0","");
+            var basePath = AppContext.BaseDirectory;
+            var path = basePath.Substring(0, basePath.Length - 18);
 
             var config = new ConfigurationBuilder()
                 .SetBasePath(path)
